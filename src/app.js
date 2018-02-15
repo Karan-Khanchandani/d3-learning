574ef456a0fb11f6ac46b2b4dfd29627db1ea4f4
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-02-15 19:40:43 +0530

    Adds transition of graph

--- src/app.js
+++ src/app.js
@@ -9,50 +9,119 @@ var margin = {
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parse = d3.timeFormat("%b %Y").parse;
+var parse = d3.timeParse("%b %Y");
 
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-var valueline = d3.line()
+var xAxis = d3.axisBottom().scale(x).tickSize(-height);
+var yAxis = d3.axisRight().scale(y).ticks(4);
+
+var area = d3.area()
+    .curve(d3.curveCatmullRom)
     .x(d => x(d.date))
-    .y(d => y(d.close));
+    .y0(height)
+    .y1(d => y(d.price));
+
+
+var line = d3.line()
+    .curve(d3.curveCatmullRom)
+    .x(d => x(d.date))
+    .y(d => y(d.price));
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transfomr", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("sample.csv", (err, data) => {
+d3.csv("data/sample.csv", type, (err, data) => {
     if (err)
         throw err;
 
-    data.forEach(function (d) {
-        d.date = parseTime(d.date);
-        d.close = +d.close;
-    });
+    var values = data.filter(d => d.symbol == "AMZN");
+   
+    var msft = data.filter(d => d.symbol = "MSFT");
 
-    x.domain(d3.extent(data, d => d.date));
-    y.domain(0, d3.max(data, d => d.close));
+    var ibm = data.filter(d => d.symbol == "IBM");
 
-    svg.append("path")
-        .data([data])
-        .attr("class", "line")
-        .attr("d", valueline);
+    x.domain([values[0].date, values[values.length - 1].date]);
+    y.domain([0, d3.max(data, d => d.price)]).nice();
+
+    svg.append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
 
     svg.append("g")
-        .attr("class", "axis")
+        .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x)
-            .tickFormat(d3.timeFormat("%Y-%m-%d")))
-        .selectAll("text")
-        .style("text-anchor", "end")
-        .attr("dx", "-.8em")
-        .attr("dy", ".15em")
-        .attr("transform", "rotate(-65)");
+        .call(xAxis)
 
     svg.append("g")
-        .attr("class", "axis")
-        .call(d3.axisLeft(y));
+        .attr("class", "y axis")
+        .attr("transform", "translate(" + width + ", 0)")
+        .call(yAxis);
+
+    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+    svg.selectAll(".line")
+        .data([values, msft, ibm])
+        .enter()
+        .append('path')
+        .attr('class', "line")
+        .style("stroke", d => colors(Math.random() * 50))
+        .attr('clip-path', 'url(#clip)')
+        .attr('d', function(d) {
+            return line(d);
+          })
+
+    var curtain = svg.append("rect")
+        .attr("x", -1 * width)
+        .attr("y", -1 * height)
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "curtain")
+        .attr("transform", "rotate(180)")
+        .style("fill", '#ffffff')
+
+    var guideline = svg.append("line")
+        .attr("stroke", "#333")
+        .attr("class", "guide")
+        .attr("stroke-width", 0)
+        .attr('x1', 1)
+    .attr("y1", 1)
+        .attr("x2", 1)
+        .attr("y2", height);
+
+    var t = svg.transition()
+        .delay(750)
+        .duration(6000)
+        .ease(d3.easeLinear)
+        .on('end', function() {
+            d3.select("line.guide")
+                .transition()
+                .style('opacity', 0)
+                .remove()
+        })
+
+    t.select('rect.curtain')
+        .attr('width', 0);
+    t.select('line.guide')
+        .attr("transform", "translate(" + width + ", 0)")
+
+    d3.select("#show_guideline").on("change", function (e) {
+        guideline.attr('stroke-width', this.checked ? 1 : 0);
+        curtain.attr("opacity", this.checked ? 0.75 : 1);
+    })
+
+
 });
+
+function type(d) {
+   
+    d.date = parse(d.date);
+   
+    d.price = +d.price;
+    return d;
+}
